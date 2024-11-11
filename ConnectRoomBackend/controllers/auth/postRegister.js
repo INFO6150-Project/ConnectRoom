d5cdd58730ef2451254fee35638bc090e7d15da5
Rithwik Srivastava <rithwiksrivastava@MacBook-Pro.local>
2024-11-11 02:50:38 -0500

    JWT token

--- ConnectRoomBackend/controllers/auth/postRegister.js
+++ ConnectRoomBackend/controllers/auth/postRegister.js
@@ -1,5 +1,6 @@
 const User = require('../../models/user'); 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const postRegister = async(req,res) => {
     try{
@@ -20,7 +21,17 @@ const postRegister = async(req,res) => {
             password: ecryptedPassword
         });
         // create JWT token 
-        const token = "JWT TOKEN"
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                mail
+            },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "24h"
+            }
+        );
+
         res.status(201).json({
             userDetails:{
             mail:  user.mail,
