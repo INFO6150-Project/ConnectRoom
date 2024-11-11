12189470da3007e358b45cb60369de8af821f20a
Rithwik Srivastava <rithwiksrivastava@MacBook-Pro.local>
2024-11-11 03:24:39 -0500

    Middleware created

--- ConnectRoomBackend/routes/authRoutes.js
+++ ConnectRoomBackend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authControllers = require('../controllers/auth/authController')
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({});
+const auth = require('../middleware/auth');
 
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
@@ -25,5 +26,10 @@ router.post('/login',
     validator.body(loginSchema),
     authControllers.controllers.postLogin
 );
+
+// test route to verify middleware
+router.get('/test', auth, (req, res) =>{
+    res.send('Request passed');
+});
 
 module.exports = router;
