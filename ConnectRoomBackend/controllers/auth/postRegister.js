b3677df0307d4aa9a3de1c660be1995035824d11
Rithwik Srivastava <rithwiksrivastava@MacBook-Pro.local>
2024-11-11 02:26:13 -0500

    Registration Functionality

--- ConnectRoomBackend/controllers/auth/postRegister.js
+++ ConnectRoomBackend/controllers/auth/postRegister.js
@@ -1,5 +1,37 @@
+const User = require('../../models/user'); 
+const bcrypt = require('bcryptjs');
+
 const postRegister = async(req,res) => {
-    res.send('register router');
+    try{
+        const{ username, mail, password } = req.body;
+
+        //check if user exists
+        const userExists = await User.exists({ mail });
+        if(userExists){
+            return res.status(409).send("Email already exists.");
+        }
+
+        const ecryptedPassword = await bcrypt.hash(password, 10);
+
+        //create user document and save in database
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: ecryptedPassword
+        });
+        // create JWT token 
+        const token = "JWT TOKEN"
+        res.status(201).json({
+            userDetails:{
+            mail:  user.mail,
+            token: token,
+            username: user.username
+            }
+        });
+    }
+    catch (err) {
+        return res.status(500).send(`Error occurred. Please try again. Details: ${err.message}`);
+    }
 };
 
 module.exports =  postRegister;
--- ConnectRoomBackend/routes/authRoutes.js
+++ ConnectRoomBackend/routes/authRoutes.js
@@ -7,12 +7,12 @@ const validator = require('express-joi-validation').createValidator({});
 const registerSchema = Joi.object({
     username: Joi.string().min(3).max(12).required(),
     password: Joi.string().min(6).max(12).required(),
-    email: Joi.string().email().required()
+    mail: Joi.string().email().required()
 })
 
 const loginSchema = Joi.object({
     password: Joi.string().min(6).max(12).required(),
-    email: Joi.string().email().required(),
+    mail: Joi.string().email().required(),
 })
 
 
