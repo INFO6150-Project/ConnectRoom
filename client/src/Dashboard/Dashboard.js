6315913b0f0562fca1ee746c259e997356a477db
Rithwik Srivastava <rithwiksrivastava@MacBook-Pro.local>
2024-11-18 14:02:56 -0500

    Added SideBar, FriendsSidebar, AddFriendButton and AddFriendDialog

--- client/src/Dashboard/Dashboard.js
+++ client/src/Dashboard/Dashboard.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { styled } from '@mui/system';
+import  SideBar  from './SideBar/SideBar';
+import FriendsSideBar from './FriendsSideBar/FriendsSideBar';
+import Messenger from './Messenger/Messenger';
+import AppBar from './AppBar/AppBar';
 
-const Dashboard = () => {
-    return <div>Dashboard</div>;
+const Wrapper = styled('div')({
+    width: '100%',
+    height: '100vh',
+    display: 'flex'
+});
+
+const Dashboard = () => { 
+    return <Wrapper>
+        <SideBar/>
+        <FriendsSideBar/>
+        <Messenger/>
+        <AppBar/>
+    </Wrapper>
 };
 
 export default Dashboard;
