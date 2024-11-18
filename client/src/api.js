e5791be9476b436c76f99f7130500215eccc109b
Mayukh Sinha <mayukh@Mayukhs-MacBook-Pro.local>
2024-11-17 20:22:23 -0500

    Added auth token with axios and logout method

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
+import { logout } from './shared/utils/auth';
+
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:5002/api',
     timeout: 1000
 });
+
+apiClient.interceptors.request.use((config) => {
+    const userDetails = localStorage.getItem('user');
+
+    if(userDetails) {
+        const token = JSON.parse(userDetails).token;
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+})
+
+//pubic routes
 
 export const login = async (data) => {
     try {
@@ -26,3 +43,14 @@ export const register = async (data) => {
         };
     }
 };
+
+//secure routes
+
+const checkResponseCode = (exception) => {
+    const responseCode = exception?.response?.status;
+
+    if(responseCode) {
+        (responseCode === 400 || responseCode === 403) && logout();
+    }
+}
+
