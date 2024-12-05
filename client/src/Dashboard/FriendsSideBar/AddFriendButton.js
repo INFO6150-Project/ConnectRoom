c603efa07fed3d5552e3f20f5f58fde74e374cdc
Mayukh Sinha <mayukh@Mayukhs-MacBook-Pro.local>
2024-12-05 03:05:24 -0500

    Bug fixes

--- client/src/Dashboard/FriendsSideBar/AddFriendButton.js
+++ client/src/Dashboard/FriendsSideBar/AddFriendButton.js
@@ -25,7 +25,7 @@ const AddFriendButton = () => {
     <>
       <CustomPrimaryButton
         additionalStyles={additionalStyles}
-        label="Add Friend"
+        label="Add Husky"
         onClick={handleOpenAddFriendDialog}
       />
       <AddFriendDialog
